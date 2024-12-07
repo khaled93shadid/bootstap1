cb7d5ecf4b9639ec907cd66a1e00a2b7c50fab5d
khaled93shadid <khaled_shadid@yahoo.com>
2024-12-07 14:50:39 +0300

    js

--- khaled.js
+++ khaled.js
@@ -1,25 +1,9 @@
-document.getElementById("khs");
-console.log(document.getElementById("khs").innerHTML);
-var title1 = document.getElementById("khs");
-title1.innerHTML="change by js"
-console.log(title1.innerHTML)
-title1.style.color="red";
-title1.style.backgroundColor="green";
-var newdiv = document.getElementById("newdiv");
-newdiv.innerHTML=`<h1>  h1 by js </h1>`;
-var h11 = document.getElementsByClassName("h11");
-console.log(h11);
-//h11[1].innerHTML=("this is from java");
-for(let i=0;i<=h11.length;i++){
-    if(i%2==0){h11[i].innerHTML='this for loop java';
-        h11[i].style.color="red";
-    }
-    else{h11[i].innerHTML='this for loop java' ;
-        h11[i].style.color="blue";
-        }
+function sumation (x,y){
 
+var z =x+y;
+console.log(z);
+return z;
 
 }
-
-
+sumation(5,6);
 
